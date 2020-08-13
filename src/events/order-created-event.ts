d18e53b66068f25cd39c1dbdef595bd54a9aaf0f
Abner Souza <abnersouza@outlook.com>
2020-08-13 15:24:09 -0300

    Adding errors handlers and middlewares

--- src/events/order-created-event.ts
+++ src/events/order-created-event.ts
@@ -1,11 +1,12 @@
-import { Subjects } from './subjects';
-import { OrderStatus } from './types/order-status';
+import { Subjects } from "./subjects";
+import { OrderStatus } from "./types/order-status";
 
 export interface OrderCreatedEvent {
   subject: Subjects.OrderCreated;
   data: {
     id: string;
     status: OrderStatus;
+    version: number;
     userId: string;
     expiresAt: string;
     ticket: {
